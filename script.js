c69a7781335c2bbb60d867190ea66a918d899889
esubel1001 <ayssomodio22@src.pshs.edu.ph>
2025-03-05 16:46:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function verifyMessage() {
+    let message = prompt("Enter the message to verify:");
+
+    if (message.includes("FR") || (message.includes("FR") && message.includes("AI"))) {
+        if (message.includes("aI") || message.includes("Ai")){  
+            document.getElementById('result').innerHTML = "The message " + '"' + message + '"' + " is fake!";
+        } else {    
+            document.getElementById('result').innerHTML = "The message " + '"' + message + '"' + " is legitimate!";
+        }
+    } else if (message.includes("AI") || message.includes("aI") || message.includes("Ai")) {
+        document.getElementById('result').innerHTML = "The message " + '"' + message + '"' + " is tampered with!";
+    } else {
+        document.getElementById('result').innerHTML = "The message " + '"' + message + '"' + " is not yet encoded.";
+    }
+}
