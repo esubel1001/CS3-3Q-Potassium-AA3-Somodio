c44f82a65261f56e22258a562af0e34f51f757b1
esubel1001 <ayssomodio22@src.pshs.edu.ph>
2025-03-05 16:48:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function verifyMessage() {
             document.getElementById('result').innerHTML = "The message " + '"' + message + '"' + " is legitimate!";
         }
     } else if (message.includes("AI") || message.includes("aI") || message.includes("Ai")) {
-        document.getElementById('result').innerHTML = "The message " + '"' + message + '"' + " is tampered with!";
+        document.getElementById('result').innerHTML = "The message " + '"' + message + '"' + " is tampered!";
     } else {
         document.getElementById('result').innerHTML = "The message " + '"' + message + '"' + " is not yet encoded.";
     }
